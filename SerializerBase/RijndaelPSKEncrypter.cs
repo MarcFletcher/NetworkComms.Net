f8e8ea595f4a308e2e09156774cb2809e1adf1e5
Matthew Dean <mcd40@cam.ac.uk>
2012-09-04 12:17:32 +0100

    Changed Rijndael encryptor to take just a password and use a fixed salt from random.org

--- SerializerBase/RijndaelPSKEncrypter.cs
+++ SerializerBase/RijndaelPSKEncrypter.cs
@@ -8,12 +8,12 @@ using System.IO;
 namespace SerializerBase
 {
     public class RijndaelPSKEncrypter : DataProcessor
-    {
-        static string IVOption = "RijndaelPSKEncrypter_IV";
-        static string KeyOption = "RijndaelPSKEncrypter_KEY";
+    {        
+        private static readonly string PasswordOption = "RijndaelPSKEncrypter_PASSWORD";
+        private static readonly byte[] SALT = new byte[] { 118, 100, 123, 136, 20, 242, 170, 227, 97, 168, 101, 177, 214, 211, 118, 137 };
 
         RijndaelManaged encrypter = new RijndaelManaged();
-
+        
         private RijndaelPSKEncrypter() { }
 
         public override byte Identifier
@@ -23,7 +23,9 @@ namespace SerializerBase
 
         public override void ForwardProcessDataStream(System.IO.Stream inStream, System.IO.Stream outStream, Dictionary<string, string> options, out long writtenBytes)
         {
-            var transform = encrypter.CreateEncryptor(StringToByteArray(options[IVOption]), StringToByteArray(options[KeyOption]));
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(options[PasswordOption], SALT);
+
+            var transform = encrypter.CreateEncryptor(pdb.GetBytes(32), pdb.GetBytes(16));
                          
             using (CryptoStream csEncrypt = new CryptoStream(outStream, transform, CryptoStreamMode.Write))
                 inStream.CopyTo(outStream);
@@ -33,53 +35,19 @@ namespace SerializerBase
 
         public override void ReverseProcessDataStream(System.IO.Stream inStream, System.IO.Stream outStream, Dictionary<string, string> options, out long writtenBytes)
         {
-            var transform = encrypter.CreateEncryptor(StringToByteArray(options[IVOption]), StringToByteArray(options[KeyOption]));
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(options[PasswordOption], SALT);
+
+            var transform = encrypter.CreateDecryptor(pdb.GetBytes(32), pdb.GetBytes(16));
 
             using (CryptoStream csEncrypt = new CryptoStream(outStream, transform, CryptoStreamMode.Write))
                 inStream.CopyTo(outStream);
 
             writtenBytes = outStream.Position; 
         }
-
-        private byte[] StringToByteArray(string IVKeyAsString)
+        
+        public static void AddPasswordToOptions(Dictionary<string, string> options, string password)
         {
-            var res = Encoding.Unicode.GetBytes(IVKeyAsString);
-
-            if (res.Length <= 16)
-            {
-                if (res.Length == 16)
-                    return res;
-
-                byte[] padded = new byte[16];
-                Buffer.BlockCopy(res, 0, padded, 0, res.Length);
-                return padded;
-            }
-            else if (res.Length <= 24)
-            {
-                if (res.Length == 24)
-                    return res;
-
-                byte[] padded = new byte[24];
-                Buffer.BlockCopy(res, 0, padded, 0, res.Length);
-                return padded;
-            }
-            else if (res.Length <= 32)
-            {
-                if (res.Length == 32)
-                    return res;
-
-                byte[] padded = new byte[32];
-                Buffer.BlockCopy(res, 0, padded, 0, res.Length);
-                return padded;
-            }
-            else
-                throw new InvalidOperationException();
-        }
-
-        public static void AddEncryptionOptions(Dictionary<string, string> options, byte[] IV, byte[] Key)
-        {
-            options[IVOption] = new string(Encoding.Unicode.GetChars(IV));
-            options[KeyOption] = new string(Encoding.Unicode.GetChars(Key));
+            options[PasswordOption] = password;
         }
     }
 }
