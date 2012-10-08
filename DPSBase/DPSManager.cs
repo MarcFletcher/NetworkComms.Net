d30f647dc7bce7442604f787e14c89878b6f218d
Matthew Dean <mcd40@cam.ac.uk>
2012-10-08 13:52:48 +0100

    Changed quickLZ so as to not cause problems with MEF on linux under mono.  Re-enabled autodetection on mono which seems to be working on mono 2.10.9

--- DPSBase/DPSManager.cs
+++ DPSBase/DPSManager.cs
@@ -36,7 +36,7 @@ namespace DPSBase
 
             static SerializerComparer() { Instance = new SerializerComparer(); }
 
-            public SerializerComparer(){}
+            public SerializerComparer() { }
 
             #region IEqualityComparer<ISerialize> Members
 
@@ -112,7 +112,7 @@ namespace DPSBase
         private Dictionary<byte, DataProcessor> DataProcessorsByID = new Dictionary<byte, DataProcessor>();
 
         static DPSManager instance;
-        
+
         static DPSManager()
         {
             instance = new DPSManager();
@@ -158,7 +158,7 @@ namespace DPSBase
         /// </summary>
         /// <returns>A dictionary whose key is the <see cref="System.Type"/> of the <see cref="DataProcessor"/> instance that is the value</returns>
         public static Dictionary<Type, DataProcessor> GetAllDataProcessors()
-        {            
+        {
             return instance.DataProcessorsByType;
         }
 
@@ -233,7 +233,7 @@ namespace DPSBase
         public static long CreateSerializerDataProcessorIdentifier(DataSerializer serializer, List<DataProcessor> dataProcessors)
         {
             long res = 0;
-                        
+
             res |= serializer.Identifier;
             res <<= 8;
 
@@ -284,63 +284,60 @@ namespace DPSBase
 
         private DPSManager()
         {
-            //Composition is not working currently on mono so we won't do it for that.
-            if (Type.GetType("Mono.Runtime") == null)
+
+            //An aggregate catalog that combines multiple catalogs
+            var catalog = new AggregateCatalog();
+
+            //We're going to try and guess where we might have serializers and compressors.  We will look in all currently loaded assemblies
+            //and all dll and exe files in the application root directory and subdirectories
+
+            //Add all the currently loaded assemblies
+            AppDomain.CurrentDomain.GetAssemblies().ToList().ForEach(ass => catalog.Catalogs.Add(new AssemblyCatalog(ass)));
+
+            var allDirectories = Directory.GetDirectories(Directory.GetCurrentDirectory(), "*", SearchOption.AllDirectories);
+
+            //Adds all the parts found in dlls in the same localtion
+            catalog.Catalogs.Add(new DirectoryCatalog(Directory.GetCurrentDirectory(), "*.dll"));
+            //Adds all the parts found in exe in the same localtion
+            catalog.Catalogs.Add(new DirectoryCatalog(Directory.GetCurrentDirectory(), "*.exe"));
+
+            for (int i = 0; i < allDirectories.Length; i++)
             {
-                //An aggregate catalog that combines multiple catalogs
-                var catalog = new AggregateCatalog();
-
-                //We're going to try and guess where we might have serializers and compressors.  We will look in all currently loaded assemblies
-                //and all dll and exe files in the application root directory and subdirectories
-
-                //Add all the currently loaded assemblies
-                AppDomain.CurrentDomain.GetAssemblies().ToList().ForEach(ass => catalog.Catalogs.Add(new AssemblyCatalog(ass)));
-
-                var allDirectories = Directory.GetDirectories(Directory.GetCurrentDirectory(), "*", SearchOption.AllDirectories);
-
-                //Adds all the parts found in dlls in the same localtion
-                catalog.Catalogs.Add(new DirectoryCatalog(Directory.GetCurrentDirectory(), "*.dll"));
-                //Adds all the parts found in exe in the same localtion
-                catalog.Catalogs.Add(new DirectoryCatalog(Directory.GetCurrentDirectory(), "*.exe"));
-
-                for (int i = 0; i < allDirectories.Length; i++)
+                if (Directory.GetFiles(allDirectories[i], "*.exe", SearchOption.TopDirectoryOnly).Union(
+                    Directory.GetFiles(allDirectories[i], "*.dll", SearchOption.TopDirectoryOnly)).Count() != 0)
                 {
-                    if (Directory.GetFiles(allDirectories[i], "*.exe", SearchOption.TopDirectoryOnly).Union(
-                        Directory.GetFiles(allDirectories[i], "*.dll", SearchOption.TopDirectoryOnly)).Count() != 0)
-                    {
-                        //Adds all the parts found in dlls in the same localtion
-                        catalog.Catalogs.Add(new DirectoryCatalog(allDirectories[i], "*.dll"));
-                        //Adds all the parts found in exe in the same localtion
-                        catalog.Catalogs.Add(new DirectoryCatalog(allDirectories[i], "*.exe"));
-                    }
+                    //Adds all the parts found in dlls in the same localtion
+                    catalog.Catalogs.Add(new DirectoryCatalog(allDirectories[i], "*.dll"));
+                    //Adds all the parts found in exe in the same localtion
+                    catalog.Catalogs.Add(new DirectoryCatalog(allDirectories[i], "*.exe"));
                 }
+            }
 
-                _container = new CompositionContainer(catalog);
+            _container = new CompositionContainer(catalog);
 
-                //Fill the imports of this object
-                try
-                {
-                    _container.ComposeParts(this);
-                }
-                catch (CompositionException compositionException)
-                {
-                    Console.WriteLine(compositionException.ToString());
-                }
+            //Fill the imports of this object
+            try
+            {
+                _container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
 
-                var serializersToAdd = serializers.Distinct(SerializerComparer.Instance).Where(s => !SerializersByType.ContainsKey(s.GetType())).ToArray();
-                var dataProcessorsToAdd = compressors.Distinct(DataProcessorComparer.Instance).Where(c => !DataProcessorsByType.ContainsKey(c.GetType())).ToArray();
+            var serializersToAdd = serializers.Distinct(SerializerComparer.Instance).Where(s => !SerializersByType.ContainsKey(s.GetType())).ToArray();
+            var dataProcessorsToAdd = compressors.Distinct(DataProcessorComparer.Instance).Where(c => !DataProcessorsByType.ContainsKey(c.GetType())).ToArray();
 
-                foreach (var instance in serializersToAdd)
-                {
-                    SerializersByType.Add(instance.GetType(), instance);
-                    SerializersByID.Add(instance.Identifier, instance);
-                }
+            foreach (var instance in serializersToAdd)
+            {
+                SerializersByType.Add(instance.GetType(), instance);
+                SerializersByID.Add(instance.Identifier, instance);
+            }
 
-                foreach (var instance in dataProcessorsToAdd)
-                {
-                    DataProcessorsByType.Add(instance.GetType(), instance);
-                    DataProcessorsByID.Add(instance.Identifier, instance);
-                }
+            foreach (var instance in dataProcessorsToAdd)
+            {
+                DataProcessorsByType.Add(instance.GetType(), instance);
+                DataProcessorsByID.Add(instance.Identifier, instance);
             }
         }
     }
