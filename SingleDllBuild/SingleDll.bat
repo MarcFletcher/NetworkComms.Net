ff1f2bfcc29f717d1c09430bb86c78760d733d37
MarcF <mfletcher@fnworldwide.com>
2012-12-21 17:54:17 +0000

    Minor changes to Documentation output, singleDLL build names and changelog fixes.

--- SingleDllBuild/SingleDll.bat
+++ SingleDllBuild/SingleDll.bat
@@ -19,9 +19,9 @@ COPY ..\Common.Logging.Log4Net.dll .\%1\Common.Logging.Log4Net.dll
 COPY ..\log4net.dll .\%1\log4net.dll
 
 md ".\%1\Complete"
-md ".\%1\Lite"
+md ".\%1\Core"
 
-.\ILMerge.exe /targetplatform:v4 /out:.\%1\Complete\NetworkCommsDotNet.dll .\%1\NetworkCommsDotNet.dll .\%1\Common.Logging.Log4Net.dll .\%1\log4net.dll .\%1\Common.Logging.dll .\%1\protobuf-net.dll .\%1\DPSBase.dll .\%1\SevenZipLZMACompressor.dll .\%1\ICSharpCode.SharpZipLib.dll .\%1\QuickLZCompressor.dll .\%1\SharpZipLibCompressor.dll .\%1\DistributedFileSystem.dll .\%1\RemoteProcedureCalls.dll
-.\ILMerge.exe /targetplatform:v4 /out:.\%1\Lite\NetworkCommsDotNet.dll .\%1\NetworkCommsDotNet.dll .\%1\Common.Logging.dll .\%1\protobuf-net.dll .\%1\DPSBase.dll .\%1\SevenZipLZMACompressor.dll
+.\ILMerge.exe /targetplatform:v4 /out:.\%1\Complete\NetworkCommsDotNetComplete_v2.0.0.dll .\%1\NetworkCommsDotNet.dll .\%1\Common.Logging.Log4Net.dll .\%1\log4net.dll .\%1\Common.Logging.dll .\%1\protobuf-net.dll .\%1\DPSBase.dll .\%1\SevenZipLZMACompressor.dll .\%1\ICSharpCode.SharpZipLib.dll .\%1\QuickLZCompressor.dll .\%1\SharpZipLibCompressor.dll .\%1\DistributedFileSystem.dll .\%1\RemoteProcedureCalls.dll
+.\ILMerge.exe /targetplatform:v4 /out:.\%1\Core\NetworkCommsDotNetCore_2.0.0.dll .\%1\NetworkCommsDotNet.dll .\%1\Common.Logging.dll .\%1\protobuf-net.dll .\%1\DPSBase.dll .\%1\SevenZipLZMACompressor.dll
 
-DEL .\%1\*.dll .\%1\Complete\*.pdb .\%1\Lite\*.pdb 
+DEL .\%1\*.dll .\%1\Complete\*.pdb .\%1\Core\*.pdb 
