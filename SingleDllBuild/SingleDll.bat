961cf3db6b629c55fbb9552353bcf28388defbf0
Matthew Dean <mcd40@cam.ac.uk>
2013-01-29 09:50:28 +0000

    Updated single dll build and removed some unused files

--- SingleDllBuild/SingleDll.bat
+++ SingleDllBuild/SingleDll.bat
@@ -1,6 +1,5 @@
 COPY ..\NetworkCommsDotNet\bin\%1\NetworkCommsDotNet.dll .\%1\NetworkCommsDotNet.dll
 
-COPY ..\DPSBase\bin\%1\protobuf-net.dll .\%1\protobuf-net.dll
 COPY ..\DPSBase\bin\%1\DPSBase.dll .\%1\DPSBase.dll
 
 COPY ..\SevenZipLZMACompressor\bin\%1\SevenZipLZMACompressor.dll .\%1\SevenZipLZMACompressor.dll
@@ -10,18 +9,17 @@ COPY ..\SharpZipLibCompressor\bin\%1\SharpZipLibCompressor.dll .\%1\SharpZipLibC
 
 COPY ..\QuickLZCompressor\bin\%1\QuickLZCompressor.dll .\%1\QuickLZCompressor.dll
 
-COPY ..\DistributedFileSystem\bin\%1\Common.Logging.dll .\%1\Common.Logging.dll
 COPY ..\DistributedFileSystem\bin\%1\DistributedFileSystem.dll .\%1\DistributedFileSystem.dll
 
 COPY ..\RemoteProcedureCalls\bin\%1\RemoteProcedureCalls.dll .\%1\RemoteProcedureCalls.dll
 
-COPY ..\Common.Logging.Log4Net.dll .\%1\Common.Logging.Log4Net.dll
-COPY ..\log4net.dll .\%1\log4net.dll
+COPY ..\DLL\Net40\protobuf-net.dll .\%1\protobuf-net.dll
+COPY ..\DLL\Net40\NLog.dll .\%1\NLog.dll
 
 md ".\%1\Complete"
 md ".\%1\Core"
 
-.\ILMerge.exe /targetplatform:v4 /out:.\%1\Complete\NetworkCommsDotNetComplete.dll .\%1\NetworkCommsDotNet.dll .\%1\Common.Logging.Log4Net.dll .\%1\log4net.dll .\%1\Common.Logging.dll .\%1\protobuf-net.dll .\%1\DPSBase.dll .\%1\SevenZipLZMACompressor.dll .\%1\ICSharpCode.SharpZipLib.dll .\%1\QuickLZCompressor.dll .\%1\SharpZipLibCompressor.dll .\%1\DistributedFileSystem.dll .\%1\RemoteProcedureCalls.dll
-.\ILMerge.exe /targetplatform:v4 /out:.\%1\Core\NetworkCommsDotNetCore.dll .\%1\NetworkCommsDotNet.dll .\%1\Common.Logging.dll .\%1\protobuf-net.dll .\%1\DPSBase.dll .\%1\SevenZipLZMACompressor.dll
+.\ILMerge.exe /targetplatform:v4 /out:.\%1\Complete\NetworkCommsDotNetComplete.dll .\%1\NetworkCommsDotNet.dll .\%1\Nlog.dll .\%1\protobuf-net.dll .\%1\DPSBase.dll .\%1\SevenZipLZMACompressor.dll .\%1\ICSharpCode.SharpZipLib.dll .\%1\QuickLZCompressor.dll .\%1\SharpZipLibCompressor.dll .\%1\DistributedFileSystem.dll .\%1\RemoteProcedureCalls.dll
+.\ILMerge.exe /targetplatform:v4 /out:.\%1\Core\NetworkCommsDotNetCore.dll .\%1\NetworkCommsDotNet.dll .\%1\Nlog.dll .\%1\protobuf-net.dll .\%1\DPSBase.dll .\%1\SevenZipLZMACompressor.dll
 
 DEL .\%1\*.dll .\%1\Complete\*.pdb .\%1\Core\*.pdb 
