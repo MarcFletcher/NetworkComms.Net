8c95a5cde7af61a1c873382219b7997ac69defb8
MarcF <unknown>
2014-03-18 12:37:56 +0000

    Corrected v3 licensing information ready for release.

--- NetworkCommsDotNet/Tools/SSLTools.cs
+++ NetworkCommsDotNet/Tools/SSLTools.cs
@@ -1,20 +1,9 @@
-﻿//  Copyright 2009-2014 Marc Fletcher, Matthew Dean
+﻿//
+//  Copyright 2009-2014 NetworkComms.Net Ltd.
 //
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+//  This source code is made available for reference purposes only.
+//  It may not be distributed and it may not be made publicly available.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//  Non-GPL versions of this software can also be purchased. 
-//  Please see <http://www.networkcomms.net> for details.
 
 #if !NETFX_CORE
 
