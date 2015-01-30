42effb3dbf1539a0edcb27adb95369e55e0730c2
MarcF <marc@echoboxapp.com>
2015-01-30 11:41:18 +0000

    Migrated protobuf serializer to unified API.

--- ProtobufSerializer/ProtobufSerializer.cs
+++ ProtobufSerializer/ProtobufSerializer.cs
@@ -16,7 +16,7 @@ using ProtoBuf.Meta;
 #if ANDROID
 using PreserveAttribute = Android.Runtime.PreserveAttribute;
 #elif iOS
-using PreserveAttribute = MonoTouch.Foundation.PreserveAttribute;
+using PreserveAttribute = Foundation.PreserveAttribute;
 #endif
 
 namespace NetworkCommsDotNet.DPSBase
